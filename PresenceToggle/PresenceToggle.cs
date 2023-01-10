799c204026c1597c2b5e5db38e1045a52787178e
art0007i <art0007i@gmail.com>
2023-01-10 19:55:05 +0100

    Add project files.

---
+++ PresenceToggle/PresenceToggle.cs
@@ -0,0 +1,66 @@
+using HarmonyLib;
+using NeosModLoader;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using FrooxEngine;
+
+namespace PresenceToggle
+{
+    public class PresenceToggle : NeosMod
+    {
+        public override string Name => "PresenceToggle";
+        public override string Author => "art0007i";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/art0007i/PresenceToggle/";
+
+        [AutoRegisterConfigKey]
+        private static readonly ModConfigurationKey<bool> KEY_DISCORD_RPC = new ModConfigurationKey<bool>("discord_rpc_enable", "If true discord rpc will be disabled", () => true);
+        [AutoRegisterConfigKey]
+        private static readonly ModConfigurationKey<bool> KEY_STEAM_RPC = new ModConfigurationKey<bool>("steam_rpc_enable", "If true steam rpc will be disabled", () => true);
+
+        private static ModConfiguration config;
+
+        public override void OnEngineInit()
+        {
+            config = GetConfiguration();
+            Harmony harmony = new Harmony("me.art0007i.PresenceToggle");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(DiscordPlatformConnector))]
+        class DiscordPlatformConnector_Patch
+        {
+            [HarmonyPrefix]
+            [HarmonyPatch("SetCurrentStatus")]
+            public static bool Prefix()
+            {
+                bool disable = config.GetValue(KEY_DISCORD_RPC);
+                if (disable)
+                {
+                    var discord = Engine.Current.PlatformInterface.GetConnectors<DiscordPlatformConnector>().First();
+                    discord.ClearCurrentStatus();
+                }
+                return !disable;
+            }
+        }
+        [HarmonyPatch(typeof(SteamConnector))]
+        class SteamPlatformConnector_Patch
+        {
+            [HarmonyPrefix]
+            [HarmonyPatch("SetCurrentStatus")]
+            public static bool Prefix()
+            {
+                bool disable = config.GetValue(KEY_STEAM_RPC);
+                if (disable)
+                {
+                    var discord = Engine.Current.PlatformInterface.GetConnectors<SteamConnector>().First();
+                    discord.ClearCurrentStatus();
+                }
+                return !disable;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# PresenceToggle
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that allows you to toggle discord and steam rich presence.<br>
+Both apps will still report that you are *playing* the game, but not what worlds you are in.
+
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+1. Place [PresenceToggle.dll](https://github.com/art0007i/PresenceToggle/releases/latest/download/PresenceToggle.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+1. Start the game. If you want to verify that the mod is working you can check your Neos logs.

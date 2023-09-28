102e49bb09b82070259b43d7a6dfbb5f9b3a0231
StiefelJackal <stiefel@jackalworkz.net>
2023-09-28 10:58:16 -0400

    feat: change name to resonite

--- PresenceToggle/PresenceToggle.cs
+++ PresenceToggle/PresenceToggle.cs
@@ -1,5 +1,5 @@
 using HarmonyLib;
-using NeosModLoader;
+using ResoniteModLoader;
 using System;
 using System.Linq;
 using System.Reflection;
@@ -9,15 +9,13 @@ using FrooxEngine;
 
 namespace PresenceToggle
 {
-    public class PresenceToggle : NeosMod
+    public class PresenceToggle : ResoniteMod
     {
         public override string Name => "PresenceToggle";
         public override string Author => "art0007i";
-        public override string Version => "1.0.0";
+        public override string Version => "2.0.0";
         public override string Link => "https://github.com/art0007i/PresenceToggle/";
 
-        [AutoRegisterConfigKey]
-        private static readonly ModConfigurationKey<bool> KEY_DISCORD_RPC = new ModConfigurationKey<bool>("discord_rpc_enable", "If true discord rpc will be disabled", () => true);
         [AutoRegisterConfigKey]
         private static readonly ModConfigurationKey<bool> KEY_STEAM_RPC = new ModConfigurationKey<bool>("steam_rpc_enable", "If true steam rpc will be disabled", () => true);
 
@@ -30,22 +28,6 @@ namespace PresenceToggle
             harmony.PatchAll();
         }
 
-        [HarmonyPatch(typeof(DiscordPlatformConnector))]
-        class DiscordPlatformConnector_Patch
-        {
-            [HarmonyPrefix]
-            [HarmonyPatch("SetCurrentStatus")]
-            public static bool Prefix()
-            {
-                bool disable = config.GetValue(KEY_DISCORD_RPC);
-                if (disable)
-                {
-                    var discord = Engine.Current.PlatformInterface.GetConnectors<DiscordPlatformConnector>().First();
-                    discord.ClearCurrentStatus();
-                }
-                return !disable;
-            }
-        }
         [HarmonyPatch(typeof(SteamConnector))]
         class SteamPlatformConnector_Patch
         {
@@ -56,8 +38,8 @@ namespace PresenceToggle
                 bool disable = config.GetValue(KEY_STEAM_RPC);
                 if (disable)
                 {
-                    var discord = Engine.Current.PlatformInterface.GetConnectors<SteamConnector>().First();
-                    discord.ClearCurrentStatus();
+                    var steamConnector = Engine.Current.PlatformInterface.GetConnectors<SteamConnector>().First();
+                    steamConnector.ClearCurrentStatus();
                 }
                 return !disable;
             }
